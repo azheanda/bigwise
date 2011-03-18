f7c210bc23177ce471df73d1b623485420d34da3
ironzezzj@gmail.com <ironzezzj@gmail.com@aa309473-b7a4-f188-2e2b-12e67ec2728c>
2011-03-18 09:50:42 +0000

    1. 添加了Controller

--- src/UI/History/HistoryController.java
+++ src/UI/History/HistoryController.java
@@ -1,8 +1,60 @@
 package BigWise.UI.History;
 import javax.swing.*;
+
+import BigWise.DataDefine.StockHistoryData;
+import BigWise.DataSpider.History.HistoryDataSpider;
+
 import java.awt.*;
 import java.awt.event.*;
-public class HistoryController {
+import java.util.Vector;
+public abstract class HistoryController implements Runnable {
+	long RefreshTime = 10000;
+	Thread t = new Thread(this);
 	
+	private static double ROWPERPAGE = 19;
+	public int total = 0;
+	public int current = 0;
+	Vector<StockHistoryData> HistoryDataList;					//   历史数据 
+	String StockCode;
+	String StockName;
+	
+	public HistoryController()
+	{
+		GetData();
+		// 算出总数，求出第一页
+		total = (int)Math.ceil(HistoryDataList.size()/ROWPERPAGE);
+		//System.out.println(total);
+		current = 1;
+		t.start();
+	}
+	
+	// 获取数据
+	public void GetData()
+	{
+		HistoryDataSpider hds = new HistoryDataSpider();
+		
+		HistoryDataList = hds.extractor("600000","", "");	
+		
+	}
+	
+	
+	public abstract void refresh();
+	
+	
+	public void run()
+	{
+		while(true)
+		{
+			try
+			{
+				GetData();
+				refresh();
+				Thread.sleep(RefreshTime);
+			}
+			catch (InterruptedException e) {
+				System.out.println("Interunpt!");
+			}
+		}
+	}
 	
 }
