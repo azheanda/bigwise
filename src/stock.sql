0dd35d0344d7d33b317e081c1a205aaa265f3fde
ironzezzj@gmail.com <ironzezzj@gmail.com@aa309473-b7a4-f188-2e2b-12e67ec2728c>
2011-03-30 10:05:33 +0000

    1.RT图

---
+++ src/stock.sql
@@ -0,0 +1,88 @@
+
+create database BigWise;
+use BigWise;
+drop table STOCKINFO;
+create table STOCKINFO (code varchar(10) not null primary key,name varchar(10),market varchar(2),industry varchar(4),province varchar(4));
+insert into STOCKINFO values("600000","浦发银行","sh","yh","sh");
+insert into STOCKINFO values("600036","招商银行","sh","yh","sh");
+insert into STOCKINFO values("601939","建设银行","sh","yh","sh");
+insert into STOCKINFO values("601328","交通银行","sh","yh","sh");
+insert into STOCKINFO values("601398","工商银行","sh","yh","sh");
+insert into STOCKINFO values("601288","农业银行","sh","yh","sh");
+insert into STOCKINFO values("000001","深发展A","sz","yh","sz");
+insert into STOCKINFO values("002142","宁波银行","sz","yh","zj");
+
+insert into STOCKINFO values("600895","张江高科","sh","fdc","sh");
+insert into STOCKINFO values("600663","陆家嘴","sh","fdc","sh");
+insert into STOCKINFO values("000002","万科A","sz","fdc","sz");
+insert into STOCKINFO values("000514","渝开发","sz","fdc","cq");
+
+insert into STOCKINFO values("601318","中国平安","sh","bx","sh");
+insert into STOCKINFO values("601601","中国太保","sh","bx","sh");
+insert into STOCKINFO values("601628","中国人寿","sh","bx","sh");
+
+insert into STOCKINFO values("600004","白云机场","sh","mhjc","sh");
+insert into STOCKINFO values("600009","上海机场","sh","mhjc","sh");
+insert into STOCKINFO values("600029","南方航空","sh","mhjc","sh");
+insert into STOCKINFO values("600115","东方航空","sh","mhjc","sh");
+insert into STOCKINFO values("000099","中信海直","sz","mhjc","sh");
+insert into STOCKINFO values("000089","深圳机场","sz","mhjc","sh");
+
+
+insert into STOCKINFO values("600598","北大荒","sh","nmsy","sh");
+insert into STOCKINFO values("600975","新五丰","sh","nmsy","sh");
+insert into STOCKINFO values("000061","农产品","sz","nmsy","sh");
+insert into STOCKINFO values("000735","罗牛山","sz","nmsy","sh");
+
+
+insert into STOCKINFO values("600981","江苏开元","sh","gjmy","sh");
+insert into STOCKINFO values("600822","上海物贸","sh","gjmy","sh");
+insert into STOCKINFO values("002091","江苏国泰","sz","gjmy","sh");
+insert into STOCKINFO values("000626","如意集团","sz","gjmy","sh");
+
+
+insert into STOCKINFO values("600377","宁沪高速","sh","gsgl","sh");
+insert into STOCKINFO values("601107","四川成渝","sh","gsgl","sh");
+insert into STOCKINFO values("000886","海南高速","sz","gsgl","sh");
+insert into STOCKINFO values("000916","华北高速","sz","gsgl","sh");
+
+
+insert into STOCKINFO values("600702","沱牌曲酒","sh","nj","sh");
+insert into STOCKINFO values("600600","青岛啤酒","sh","nj","sh");
+insert into STOCKINFO values("000568","泸州老窖","sz","nj","sh");
+insert into STOCKINFO values("000596","古井贡酒","sz","nj","sh");
+
+
+insert into STOCKINFO values("600581","八一钢铁","sh","gt","sh");
+insert into STOCKINFO values("600282","南钢股份","sh","gt","js");
+insert into STOCKINFO values("000709","河北钢铁","sz","gt","hb");
+insert into STOCKINFO values("000959","首钢股份","sz","gt","bj");
+
+
+insert into STOCKINFO values("601857","中国石油","sh","sy","sh");
+insert into STOCKINFO values("600028","中国石化","sh","sy","sh");
+insert into STOCKINFO values("000985","大庆华科","sz","sy","sh");
+insert into STOCKINFO values("000698","沈阳化工","sz","sy","sh");
+
+
+insert into STOCKINFO values("600116","三峡水利","sh","dl","sh");
+insert into STOCKINFO values("600021","上海电力","sh","dl","sh");
+insert into STOCKINFO values("000883","湖北能源","sz","dl","sh");
+insert into STOCKINFO values("000695","滨海能源","sz","dl","sh");
+
+
+insert into STOCKINFO values("601519","大智慧","sh","dzxx","sh");
+insert into STOCKINFO values("600059","东方财富","sh","dzxx","sh");
+insert into STOCKINFO values("000021","长城开发","sz","dzxx","sh");
+insert into STOCKINFO values("000066","长城电脑","sz","dzxx","sh");
+
+
+insert into STOCKINFO values("601919","中国远洋","sh","gksy","sh");
+insert into STOCKINFO values("601880","大连港","sh","gksy","sh");
+insert into STOCKINFO values("002040","南京港","sz","gksy","sh");
+insert into STOCKINFO values("000507","珠海港","sz","gksy","sh");
+
+
+insert into STOCKINFO values("600522","中天科技","sh","txhy","sh");
+insert into STOCKINFO values("600050","中国联通","sh","txhy","sh");
+
